6c94b93232fd0ac753b9fcd5d137f7b30f280b4e
BackSam <47339652+BackSam6348@users.noreply.github.com>
2026-01-04 20:53:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ fetch("posts.json")
   .then(r => r.json())
   .then(posts => {
     const pinned = posts.filter(p => p.pinned);
-    const normal = posts.filter(p => !p.pinned);
+    const normal = posts.filter(p => !p.pinned).reverse();
 
-    [...pinned, ...normal.reverse()].forEach(post => {
+    [...pinned, ...normal].forEach(post => {
       const div = document.createElement("div");
-      div.className = "post";
+      div.className = "post clickable";
+
+      div.onclick = () => {
+        location.href = `post.html?id=${post.id}`;
+      };
 
       div.innerHTML = `
         ${post.pinned ? '<div class="pin">📌 공지</div>' : ""}
         <h2>${post.title}</h2>
         <div class="date">${post.date}</div>
-        ${
-          post.image
-            ? `<img src="${post.image}" class="preview">`
-            : ""
-        }
-        <p>${post.content.replace(/\n/g, "<br>")}</p>
+        ${post.image ? `<img src="${post.image}" class="preview">` : ""}
+        <p>${marked.parse(post.content.split("\n").slice(0, 3).join("\n"))}</p>
       `;
 
       document.getElementById("posts").appendChild(div);
