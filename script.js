e43d23fc5cd98d1c3f6f772baa355d4caee7a55f
BackSam <47339652+BackSam6348@users.noreply.github.com>
2026-01-04 20:52:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 fetch("posts.json")
-  .then(res => res.json())
+  .then(r => r.json())
   .then(posts => {
-    posts
-      .slice()
-      .reverse()
-      .forEach(post => {
-        const div = document.createElement("div");
-        div.className = "post";
-        div.innerHTML = `
-          <h2>${post.title}</h2>
-          <div class="date">${post.date}</div>
-          <p>${post.content.replace(/\n/g, "<br>")}</p>
-        `;
-        document.getElementById("posts").appendChild(div);
-      });
+    const pinned = posts.filter(p => p.pinned);
+    const normal = posts.filter(p => !p.pinned);
+
+    [...pinned, ...normal.reverse()].forEach(post => {
+      const div = document.createElement("div");
+      div.className = "post";
+
+      div.innerHTML = `
+        ${post.pinned ? '<div class="pin">📌 공지</div>' : ""}
+        <h2>${post.title}</h2>
+        <div class="date">${post.date}</div>
+        ${
+          post.image
+            ? `<img src="${post.image}" class="preview">`
+            : ""
+        }
+        <p>${post.content.replace(/\n/g, "<br>")}</p>
+      `;
+
+      document.getElementById("posts").appendChild(div);
+    });
   });
